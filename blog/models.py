36791b9234805d6979d81594f77ea65b12846e97
Redtonny <emmanueldago57@gmail.com>
2025-11-23 13:27:38 +0000

    migrations du model et insertion des données

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tutoriales(models.Model):
+    tutoriales_title= models.CharField(max_length=200)
+    tutoriales_content= models.TextField()
+    tutoriales_published= models.DateTimeField("Date published")
+    
+    def __str__(self):
+        return self.tutoriales_title
