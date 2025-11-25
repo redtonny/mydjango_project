97a2472dde74a7178dda2ae8fc1812a77684cfe7
Redtonny <emmanueldago57@gmail.com>
2025-11-25 16:24:19 +0000

    creation et encapsulation des nvos fichiers, creation de message et amenagement de la navbar

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Tutoriales
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 def index(request):
     return render(request=request,
                   template_name="blog/home.html",
@@ -13,11 +14,14 @@ def register(request):
        form=UserCreationForm(request.POST)
        if form.is_valid():
            user = form.save()
+           username= form.cleaned_data.get('username')
+           messages.success(request, f"New account created: {username}")
            login(request, user)
+           messages.info(request, f"You are now logged is a: {username}")
            return redirect('blog:index')
        else:
-           print(form.errors)
-    
+           for msg in form.error_messages:
+               messages.error(request, f"{msg}:{form.error_messages}")
    else:
         form = UserCreationForm()
      
